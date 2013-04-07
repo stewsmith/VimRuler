ab526cd909ec9aee118187594ac26b9127354601
Alex Tang <alex.m.tang@gmail.com>
2013-04-07 00:20:22 -0400

    asdfasdf

--- server.js
+++ server.js
@@ -3,5 +3,14 @@ var express = require('express')
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server);
 
+app.get('/', function(req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
 
+io.sockets.on('connection', function(socket) {
+
+
+});
+
+server.listen(8080);
 
