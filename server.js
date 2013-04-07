b9e010f8de2f5623cd4cf875cdeb8249232dee59
Stewart <stewartLsmith12@gmail.com>
2013-04-07 01:34:57 -0400

    moved around files

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express')
   , app = express()
   , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server);
+  , io = require('socket.io').listen(server)
+  , path = require('path');
 
+app.use(express.static(path.join(__dirname, '/public')));
 app.get('/', function(req, res) {
   res.sendfile(__dirname + '/index.html');
 });
